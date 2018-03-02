c880c4e86bcaf242c489ef049679ba35ab090e5f
czi <czi@MaecMini.local>
2018-03-02 09:54:46 +0100

    Minor modifications

--- clocGits.sh
+++ clocGits.sh
@@ -8,28 +8,28 @@
 
 # check if parameters are passed
 if [ $# -eq 0 ]; then
-	echo 'Usage: ' $0 ' DOWNLOADFOLDER USERNAME [ORGANIZATIONNAME]'
+	echo 'Usage: ' $0 ' DOWNLOADFOLDERNAME USERNAME [ORGANIZATIONNAME]'
 	exit 1
 fi
 
 # variables
-FOLDER=$1
+DOWNLOADFOLDERNAME=$1
 USERNAME=$2
 ORGANIZATIONNAME=$3
 PATH_GITIM='./gitim'
-PATH_CLOC='./cloc/cloc'
+PATH_CLOC='cloc/cloc'
 
 LOGFILE='results.log'
 
 # clone git repositories with gitim
 if [ -z "$3" ]; then
-	cd $PATH_GITIM; python3 -m gitim -u $USERNAME -d $FOLDER; cd ..
+	cd $PATH_GITIM; python3 -m gitim -u $USERNAME -d ../$DOWNLOADFOLDERNAME; cd ..
 else
- 	cd $PATH_GITIM; python3 -m gitim -u $USERNAME -o $ORGANIZATIONNAME -d $FOLDER; cd ..
+ 	cd $PATH_GITIM; python3 -m gitim -u $USERNAME -o $ORGANIZATIONNAME -d ../$DOWNLOADFOLDERNAME; cd ..
 fi
 
 # retrieve LOC with cloc
-for d in ./$FOLDER/*/ ; do (cd "$d" && echo "$d" && ../../$PATH_CLOC --vcs git); done > ../../$LOGFILE; cd ../..
+for d in ./$DOWNLOADFOLDERNAME/*/ ; do (cd "$d" && echo "$d" && ./../../$PATH_CLOC --vcs git); done > $LOGFILE
 
 # calculate LOC
 ALLLOC=`cat $LOGFILE | grep SUM | awk -F ' ' '{sum+=$5} END {print sum}'`
@@ -38,6 +38,8 @@ COMMENTS=`cat $LOGFILE | grep SUM | awk -F ' ' '{sum+=$4} END {print sum}'`
 
 result=`expr $ALLLOC - $BLANKS - $COMMENTS`
 
+echo 'Logfile: \n'
 cat $LOGFILE
-
+echo '================================================================'
+echo 'Calculated Stats:'
 echo 'LOC ['$1'./*] (without comments and blanks):' $result
